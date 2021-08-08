c249b6d2538ab65be52a11c820e4f32956b44e40
ahsan722505 <ahsantahir722505@gmail.com>
2021-08-08 17:48:38 +0500

    third

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
 const path = require("path");
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const Api = require("./routes/api");
 
-const MONGODB_URI =
-  "mongodb+srv://ahsan88:ahsan123@cluster0.dtvgd.mongodb.net/socialDB?retryWrites=true&w=majority";
+const Api = require("./routes/api");
 
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: "sessions",
-});
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(
-  session({
-    secret: "my secret",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
 
 app.use("/api", Api);
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(process.env.MONGODB_URI)
   .then((result) => {
-    app.listen(8080);
+    app.listen(process.env.PORT || 8080);
   })
   .catch((err) => {
     console.log(err);
